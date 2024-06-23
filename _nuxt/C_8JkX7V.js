f8397fa3285aafc40c9762a2f0869bd967ef10af
German Dauliash <regxxxman@gmail.com>
2024-06-23 20:54:05 +0200

    new version

---
+++ _nuxt/C_8JkX7V.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./DDI_y0Y3.js";import{d as t,o as n,z as _}from"./D-fj5dzd.js";const m=t({__name:"services",setup(c){return(r,s)=>{const e=o;return n(),_(e)}}});export{m as default};
